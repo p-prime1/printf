4fc6b2bbecafb0e2500896a92449e45989124a1e
p-prime1 <preciousnwaka95@gmail.com>
2023-10-18 22:32:50 +0100

    Added the print_string prototype to heaer file

--- header.h
+++ header.h
@@ -2,12 +2,15 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_string(const char *s);
 
 
 #endif
